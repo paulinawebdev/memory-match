c1d265c96534445afc6643722c353d880bb65e85
Paulina C <paulinachee.is@gmail.com>
2019-04-08 10:36:54 -0700

    This version is almost complete I added a lot of css for the about page and main page I added functionality for the winning popup Completed the stats functionality

---
+++ main.js
@@ -0,0 +1,220 @@
+$(document).ready(initializeApp);
+
+function initializeApp() {
+
+    populateCards(imgArrayMain);
+
+    displayStats();
+
+    $(".card").on("click", function() {
+        var card_click = $(this);
+        cardClicked(card_click);
+    });
+
+    $("#reset-btn").on("click", function() {
+        resetStats();
+    });
+
+    $("#close-popup").on("click", function() {
+        $(".winning-popup").removeClass("show");
+    });
+
+    $("#tryagain-btn").on("click", function() {
+        tryAgainClick();
+    });
+
+    popupStan()
+    
+}
+
+
+var imgArrayMain = ["batman-1.jpg", "batman-2.jpg","captainamerica-1.jpg", "captainamerica-2.jpg", "flash-1.jpg", "flash-2.jpg", "harleyquinn-1.jpg", "harleyquinn-2.jpg", "hulk-1.jpg", "hulk-2.jpg", "ironman-1.jpg", "ironman-2.jpg", "spiderman-1.jpg", "spiderman-2.jpg", "superman-1.jpg", "superman-2.jpg", "venom-1.jpg", "venom-2.jpg"];
+var imgArrayPop = ["batman-1.jpg", "batman-2.jpg","captainamerica-1.jpg", "captainamerica-2.jpg", "flash-1.jpg", "flash-2.jpg", "harleyquinn-1.jpg", "harleyquinn-2.jpg", "hulk-1.jpg", "hulk-2.jpg", "ironman-1.jpg", "ironman-2.jpg", "spiderman-1.jpg", "spiderman-2.jpg", "superman-1.jpg", "superman-2.jpg", "venom-1.jpg", "venom-2.jpg"];
+
+var first_card_clicked = null;
+var second_card_clicked = null;
+var total_possible_matches = (imgArrayMain.length / 2);
+var matches = 0;
+var attempts = 0;
+var accuracy = 0;
+var games_played = 0;
+
+var first_card_src;
+var second_card_src;
+
+var attempts_val;
+var matches_val;
+var accuracy_val;
+var games_val;
+
+
+function populateCards(imgArray) {
+
+    var card_max_cols = 6; // should be divisible by 18 for easy mode
+    var game_area = $("#game-area");
+
+    for (var col_count = 0; col_count < card_max_cols; col_count++) {
+
+        var card_col = $("<div>").addClass("card-col");
+
+        game_area.append(card_col);
+
+        for (var card_count = 0; card_count < Math.ceil(imgArrayMain.length / card_max_cols); card_count++ ) {
+
+            var random_index = Math.floor(Math.random() * imgArrayPop.length)
+            var random_card = imgArrayPop[random_index];
+
+            imgArrayPop.splice(random_index, 1);
+
+            var card_container = $("<div class='card'>");
+            var card_inside = $("<div class='card-inside'>");
+            var card_img_back = $("<img>").attr("src", "img/card-back1.jpg");
+            var card_img_front = $("<img>").attr("src", "img/" + random_card);
+            var card_front = $("<div class='card-front'>").append(card_img_front);
+            var card_back = $("<div class='card-back'>").append(card_img_back);
+    
+            card_container.append(card_inside);
+            card_inside.append(card_front, card_back);
+            card_col.append(card_container);
+
+        }
+
+    }
+}
+
+function cardClicked(card_click) {
+
+    var card_img = card_click.find("img");
+    var card_src = $(card_img[0]).attr("src");
+    
+    card_src = card_src.split("/");
+    card_src = card_src[1].split("-");
+    card_src = card_src[0];
+
+    if ($(card_click).attr('class') === "card reveal") {
+        return;
+    }
+
+    if (first_card_clicked === null) {
+
+        first_card_src = card_src;
+        first_card_clicked = card_click;
+
+        $(first_card_clicked).addClass("reveal");
+
+
+    } else if (second_card_clicked === null) {
+        
+        second_card_src = card_src;
+        second_card_clicked = card_click;
+
+        $(second_card_clicked).addClass("reveal");
+
+        attempts++;
+
+        displayStats();
+
+        if (first_card_src === second_card_src) {
+
+            matches++;
+
+            displayStats();
+
+            var first_card_click_inside = $(first_card_clicked).find(".card-inside");
+            var second_card_click_inside = $(second_card_clicked).find(".card-inside");
+
+            first_card_click_inside.addClass("match").find(".card-front, .card-back").addClass("no-shadow");
+            second_card_click_inside.addClass("match").find(".card-front, .card-back").addClass("no-shadow");
+
+            first_card_clicked = null;
+            second_card_clicked = null;
+
+            if (matches === total_possible_matches) {
+
+                setTimeout(function(){
+                    winningDisplay()
+                }, 500)
+
+            } //else {
+                //return something here
+            //}
+
+            return;
+
+        } else {
+            
+            setTimeout(function(){
+                
+                $(first_card_clicked).removeClass("reveal");
+                $(second_card_clicked).removeClass("reveal");
+
+                first_card_clicked = null;
+                second_card_clicked = null;
+
+            }, 1000)
+        }
+
+    }
+}
+
+function displayStats() {
+    attempts_val = $(".attempts .value");
+    matches_val = $(".matches .value");
+    accuracy_val = $(".accuracy .value");
+    games_val = $(".games-played .value");
+
+    accuracy = (matches / attempts) * 100;
+
+    if (isNaN(accuracy)) {
+        accuracy = 0;
+    }
+
+    if (accuracy < 30) {
+        $("#popup-submsg").text("But that took a lot of tries...");
+    } else {
+        $("#popup-submsg").text("You're pretty good...");
+    }
+
+    attempts_val.text(attempts);
+    matches_val.text(matches);
+    accuracy_val.text(accuracy.toFixed(0) + "%");
+    games_val.text(games_played);
+}
+
+function resetStats() {
+    games_played++;
+    games_val.text(games_played);
+
+    accuracy = 0;
+    matches = 0;
+    attempts = 0;
+
+    displayStats()
+    resetCards()
+}
+
+function resetCards() {
+    $(".card").removeClass("reveal");
+    $(".card-inside").removeClass("match winning");
+}
+
+function winningDisplay() {
+    $(".card-inside").addClass("winning");
+    $(".winning-popup").addClass("show");
+}
+
+function tryAgainClick() {
+    $(".winning-popup").removeClass("show");
+    resetStats()
+}
+
+function popupStan() {
+    setInterval(function(){ 
+        var randomX = Math.random() * 100;
+        $(".stanlee-popup").css({"opacity" : 1, "left": randomX + "%" });
+
+        setTimeout(function() {
+            $(".stanlee-popup").css("opacity", 0);
+        }, 1000);
+    }, 8000);
+}
